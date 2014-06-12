19ef1f00a74eac32bfc594f1c7249f96a106f3e1
SherryShuranHuang <huangsr6@bu.edu>
2014-06-12 14:51:31 -0400

    add gl view port

--- Project1/Project1/HelloWorld.cpp
+++ Project1/Project1/HelloWorld.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-
-int main()
-{
-	std::cout << "Hello, World!\n";
-	std::cin.get();
-	return 0;
-}
+//#include<iostream>
+//
+//int main()
+//{
+//	std::cout << "Hello, World!\n";
+//	std::cin.get();
+//	return 0;
+//}
