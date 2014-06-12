7050c94e6893e89be98f7b299bea95581c1b4570
Joe Mok <joe_g16@hotmail.com>
2014-06-12 15:29:08 -0700

    cout to david's branch

--- Project1/Project1/HelloWorld.cpp
+++ Project1/Project1/HelloWorld.cpp
@@ -6,7 +6,7 @@
 
 //int main()
 //{
-//	std::cout << "Hello, World!\n" << "This is Master's Branch" << std::endl;
+//	std::cout << "Hello, World!\n" << "This is David's Branch" << std::endl;
 //	std::cin.get();
 //	return 0;
 //}
