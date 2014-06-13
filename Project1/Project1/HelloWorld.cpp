6becb781abdc413a940541dac6e43d83945969e5
Joe Mok <joe_g16@hotmail.com>
2014-06-12 17:42:01 -0700

    Revert shit

--- Project1/Project1/HelloWorld.cpp
+++ Project1/Project1/HelloWorld.cpp
@@ -3,6 +3,10 @@
 int main()
 {
 	std::cout << "Hello, World!\n" << "This is Joe's Branch" << std::endl;
+<<<<<<< HEAD
 	std::cin.get();adsbgakjsdnfkanfal
+=======
+	std::cin.get();
+>>>>>>> parent of 7906016... freeglut stuff
 	return 0;
 }
