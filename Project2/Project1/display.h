afcf5f16cd08c8bb35ad13487ea9f9f04a5b9e4a
David <davidklaus0@gmail.com>
2014-06-17 14:50:10 -0400

    display.h

--- Project2/Project1/display.h
+++ Project2/Project1/display.h
@@ -7,12 +7,13 @@ class Display
 {
 public:
 	Display(int width, int height, const std::string& title);
-	virtual ~Display();
-	void Update();
-	bool IsClosed(){ return m_isClosed; }
+	~Display();
+
+	void Clear(float r, float g, float b, float a);
+	void swapBuffers();
+	bool isClosed();
 
 private:
-
 	SDL_Window* m_window;
 	SDL_GLContext m_glContext;
 	bool m_isClosed;
