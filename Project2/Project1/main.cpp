34e9d8b969c5a8190ed708b5424c0cfa7c6bc2be
David <davidklaus0@gmail.com>
2014-06-17 14:44:28 -0400

    main.cpp

--- Project2/Project1/main.cpp
+++ Project2/Project1/main.cpp
@@ -1,9 +1,20 @@
+#include <GL/glew.h>
+#include <SDL/SDL.h>
 #include <iostream>
 #include "display.h"
 
-int main()
+
+int main(int argc, char** argv)
 {
-	Display display(800, 600, "Hello World");
-	system("pause");
+	Display W(800, 600, "Window");
+
+	while (!W.isClosed()){
+
+		W.Clear(0.0f, 1.0f, 0.3f, 0.5f);
+
+		W.swapBuffers();
+	}
+
+	std::cin.get();
 	return 0;
 }
