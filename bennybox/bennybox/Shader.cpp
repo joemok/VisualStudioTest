b3ee6aba4a7ffb1b0e31a682cf6fa5621ad5ef1e
David <davidklaus0@gmail.com>
2014-06-19 14:12:12 -0400

    Added hidden surface elimination

--- bennybox/bennybox/Shader.cpp
+++ bennybox/bennybox/Shader.cpp
@@ -8,8 +8,8 @@
 Shader::Shader(const std::string& fileName)
 {
 	m_program = glCreateProgram();
-	m_shaders[0] = CreateShader(LoadShader(fileName + ".vs"), GL_VERTEX_SHADER);
-	m_shaders[1] = CreateShader(LoadShader(fileName + ".fs"), GL_FRAGMENT_SHADER);
+	m_shaders[0] = CreateShader(LoadShader(fileName + ".vert"), GL_VERTEX_SHADER);
+	m_shaders[1] = CreateShader(LoadShader(fileName + ".frag"), GL_FRAGMENT_SHADER);
 
 	for (unsigned int i = 0; i < NUM_SHADERS; i++)
 		glAttachShader(m_program, m_shaders[i]);
--- bennybox/bennybox/main.cpp
+++ bennybox/bennybox/main.cpp
@@ -51,7 +51,7 @@ int main(int argc, char** argv)
 		monkey.Draw();
 
 		display.swapBuffers();
-		counter += 0.001f;
+		counter += 0.01f;
 	}
 
 	std::cin.get();
