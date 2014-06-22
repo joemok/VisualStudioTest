627ce8090d3b002e6c883fa4dc412a3e74a002aa
AlbertYuChen <chenyu610103@gmail.com>
2014-06-21 20:35:48 -0400

    add bunnybox

--- Project1/Project1/Display.h
+++ Project1/Project1/Display.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <SDL2\SDL.h>
+#include <SDL\SDL.h>
 
 class Display
 {
@@ -9,6 +9,6 @@ public:
 	Display(int width, int height, const std::string& name);
 	~Display();
 private:
-	SDL_Window* m_window;
+	//SDL_Window* m_window;
 };
 
